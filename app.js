ad1e43ac46588cb90c207b8122168dbf71eb541c
sairam <mr.sairamofficial@gmail.com>
2025-07-19 11:02:04 +0530

    crud operations

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const port = 8080;
 const Listing = require("./models/listing.js");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -10,28 +14,43 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-main().then(()=>{
+main().then(() => {
     console.log("Connected sucessfully");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err)
 });
 
-app.get("/",(req,res) =>{
+app.get("/", (req, res) => {
     res.send(`App is listening at port : ${port}`)
 })
 
-app.get("/test", async (req,res)=>{
-    let sampleListing = new Listing({
-        title:"My new Villa",
-        description: "This is description",
-        price:1200,
-        location: "canada",
-    })
-    await sampleListing.save();
-    console.log ("Sample was saved");
-    res.send("Successful testingg");
+// app.get("/test", async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title:"My new Villa",
+//         description: "Thi is my new Villa in Goa!!",
+//         price:1200,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     })
+//     await sampleListing.save();
+//     console.log ("Sample was saved");
+//     res.send("Successful testingg");
+// })
+
+app.listen(port, (req, res) => {
+    console.log(`App is listening at port : ${port}`);
+});
+
+
+// index route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", { allListings });
 })
 
-app.listen(port,(req,res)=>{
-    console.log(`App is listening at port : ${port}`);
+// read/show route
+app.get("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    let item = await Listing.findById(id);
+    res.render("./listings/show.ejs",{item})
 })
--- models/listing.js
+++ models/listing.js
@@ -1,16 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// First, define the image schema
+const imageSchema = new Schema({
+    filename: String,
+    url: String
+}, { _id: false }); // disables _id for subdocument
+
 const listingSchema = new Schema({
     title: {
         type: String,
-        required:true,
+        required: true,
     },
     description: String,
     image: {
-        type: String,
-        default:"https://images.unsplash.com/photo-1751575004364-a983e57bb428?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === "" ? "https://images.unsplash.com/photo-1751575004364-a983e57bb428?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        type: imageSchema,
+        default: {
+            filename: "defaultImage",
+            
+            url: "https://images.unsplash.com/photo-1751575004364-a983e57bb428?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1751575004364-a983e57bb428?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        }
     },
     price: Number,
     location: String,
@@ -18,5 +29,5 @@ const listingSchema = new Schema({
 
 });
 
-const Listing = mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
